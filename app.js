97ad1a63cadb62c858aa7daf1185555966cfc0cd
srijani07 <srijaniparui07@gmail.com>
2021-08-06 14:19:22 +0530

    styling : removed image

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var inputOne = document.querySelector("#input-one");
 var inputTwo = document.querySelector("#input-two");
 var inputThree = document.querySelector("#input-three");
 var btnCheck = document.querySelector("#btn-check");
-
 var outputArea = document.querySelector("#output-area");
+
 outputArea.style.display = "none";
+
 function clickHandler() {
 
 if(parseInt(inputOne.value) <=0 || parseInt(inputOne.value) >=180 || parseInt(inputTwo.value) <=0 || parseInt(inputTwo.value) >=180 || parseInt(inputThree.value) <=0 || parseInt(inputThree.value) >=180  || inputOne.value === "" || inputTwo.value === "" ||inputThree.value === "" ){ //checking if angles are between 1 and 179 or not
@@ -13,12 +14,14 @@ if(parseInt(inputOne.value) <=0 || parseInt(inputOne.value) >=180 || parseInt(in
 }
 
 else {
-if(parseInt(inputOne.value) + parseInt(inputTwo.value)  + parseInt(inputThree.value) === 180)
+if(parseInt(inputOne.value) + parseInt(inputTwo.value)  + parseInt(inputThree.value) === 180) {
     outputArea.innerHTML = "Yes, these angles can form a triangle.";
-
+   
+}
 
 else 
     outputArea.innerHTML = "No, these angles cannot form a triangle.";
+    
 
 }
 outputArea.style.display = "block";
