3788f6dc928557b6e8b8f314ad4dfd61c536cde7
srijani07 <srijaniparui07@gmail.com>
2021-08-06 13:55:36 +0530

    styling : updated output area display

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var inputThree = document.querySelector("#input-three");
 var btnCheck = document.querySelector("#btn-check");
 
 var outputArea = document.querySelector("#output-area");
+outputArea.style.display = "none";
 function clickHandler() {
 
 if(parseInt(inputOne.value) <=0 || parseInt(inputOne.value) >=180 || parseInt(inputTwo.value) <=0 || parseInt(inputTwo.value) >=180 || parseInt(inputThree.value) <=0 || parseInt(inputThree.value) >=180  || inputOne.value === "" || inputTwo.value === "" ||inputThree.value === "" ){ //checking if angles are between 1 and 179 or not
@@ -20,5 +21,6 @@ else
     outputArea.innerHTML = "No, these angles cannot form a triangle.";
 
 }
+outputArea.style.display = "block";
 }
 btnCheck.addEventListener("click", clickHandler);
